fb23c08780c14e9a1caaffab8aeb9f4e624be485
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2024-03-30 11:24:42 -0400

    Time: 56 ms (58.19%) | Memory: 52.2 MB (37.30%) - LeetSync

--- 792-binary-search/binary-search.js
+++ 792-binary-search/binary-search.js
@@ -4,7 +4,16 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    
+    // let lo = 0, hi = nums.length-1;
+    // while (lo < hi) {
+    //     let mid = lo + Math.floor((hi-lo+1)/2);
+    //     if (target < nums[mid]) {
+    //         hi = mid - 1
+    //     } else {
+    //         lo = mid; 
+    //     }
+    // }
+    // return nums[lo]==target?lo:-1;
     let low = 0
     let high = nums.length - 1
     while (low < high) {
