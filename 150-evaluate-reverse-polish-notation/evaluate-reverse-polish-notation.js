2f59f06dd80b62e0736e373d9e47dd2c2bccbc66
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2024-04-02 15:27:33 -0400

    Time: 385 ms (10.56%) | Memory: 60.2 MB (6.96%) - LeetSync

---
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.js
@@ -0,0 +1,41 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function(tokens) {
+    let stack = []
+    for (let num of tokens) {
+        let ans = 0;
+        if (num !== "+" &&
+            num !== "-" &&
+            num !== "*" &&
+            num !== "/") {
+                stack.push(num)
+                console.log(stack)
+        } else {
+            if (num === "+") {
+                ans = parseInt(stack[stack.length - 2]) + parseInt(stack[stack.length - 1])
+                stack.pop()
+                stack.pop()
+                stack.push(ans)
+            } else if (num === "-") {
+                ans = parseInt(stack[stack.length - 2]) - parseInt(stack[stack.length - 1])
+                stack.pop()
+                stack.pop()
+                stack.push(ans)
+            } else if (num === "/") {
+                ans = Math.trunc(parseInt(stack[stack.length - 2]) / parseInt(stack[stack.length - 1]))
+                console.log(ans)
+                stack.pop()
+                stack.pop()
+                stack.push(ans)
+            } else {
+                ans = parseInt(stack[stack.length - 2]) * parseInt(stack[stack.length - 1])
+                stack.pop()
+                stack.pop()
+                stack.push(ans)
+            }
+        }
+    }
+    return stack[0]
+};
