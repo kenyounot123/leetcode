e63ed7eb1d331d4276c6c6aec9a90a98d11371e1
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2024-04-28 12:06:40 -0400

    Time: 57 ms (32.69%) | Memory: 49.3 MB (33.85%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.js
+++ 226-invert-binary-tree/invert-binary-tree.js
@@ -17,10 +17,11 @@ var invertTree = function(root) {
         return null
     }
 
+    invertTree(root.left)
+    invertTree(root.right)
+    
     const tmp = root.left
     root.left = root.right
     root.right = tmp
-    invertTree(root.left)
-    invertTree(root.right)
     return root
 };
