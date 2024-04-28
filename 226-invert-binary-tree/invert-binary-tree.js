6e259a6d4ca0f9c94ea485e987ff49866d8920d6
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2024-04-28 11:50:23 -0400

    Time: 64 ms (6.29%) | Memory: 49.4 MB (23.38%) - LeetSync

---
+++ 226-invert-binary-tree/invert-binary-tree.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    // recursively go traverse tree depth first
+    // while doing so, invert node.right and node.left
+    if (!root) {
+        return null
+    }
+
+    const tmp = root.left
+    root.left = root.right
+    root.right = tmp
+    invertTree(root.left)
+    invertTree(root.right)
+    return root
+};
