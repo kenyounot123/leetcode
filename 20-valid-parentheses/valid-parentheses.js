6ec13191b202466f270f8bc8ad419f6c49f0fec1
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2024-05-03 20:28:26 -0400

    Time: 53 ms (79.51%) | Memory: 50.5 MB (38.78%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.js
+++ 20-valid-parentheses/valid-parentheses.js
@@ -4,7 +4,7 @@
  */
 var isValid = function(s) {
     let stack = [];
-    for (char of s) {
+    for (let char of s) {
         if (char === "(" || char === "[" || char === "{") {
             stack.push(char)
         } else {
@@ -12,11 +12,10 @@ var isValid = function(s) {
                 (char === ")" && stack[stack.length - 1] !== "(") ||
                 (char === "]" && stack[stack.length - 1] !== "[") ||
                 (char === "}" && stack[stack.length - 1] !== "{")) {
-                return false;
-            } 
+                return false
+            }
             stack.pop();
-            
         }
     }
-    return !stack.length;
+    return !stack.length
 };
