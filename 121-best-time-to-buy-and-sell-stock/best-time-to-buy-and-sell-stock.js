5ad1ccc528a47e57599a0582456182fc0f6a6110
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2024-05-15 19:21:28 -0400

    Time: 77 ms (43.31%) | Memory: 58.8 MB (69.28%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -3,20 +3,15 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let left = 0;
-    let right = 1;
-    let maxProfit = 0
-    while (right < prices.length) {
-        
-        if (prices[right] < prices[left]) {
-            left = right
-            right++
-        } 
-        else {
-            const profit = prices[right] - prices[left]
-            maxProfit = Math.max(maxProfit, profit)
-            right++
+    let max = 0;
+
+    for (let i = 0; i < prices.length; i++) {
+        let r = i + 1;
+        while (prices[i] < prices[r]) {
+            const profit = prices[r] - prices[i]
+            max = Math.max(profit, max)
+            r++
         }
     }
-    return maxProfit
-};
+    return max
+}
