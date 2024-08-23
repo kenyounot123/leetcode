1b854ddebedf4cdeeea68aeedad10a26622ed45f
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2024-08-22 23:19:49 -0400

    Time: 58 ms (25.01%) | Memory: 48.7 MB (82.62%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.js
+++ 74-search-a-2d-matrix/search-a-2d-matrix.js
@@ -3,13 +3,49 @@
  * @param {number} target
  * @return {boolean}
  */
+
+/*
+Given m x n integer matrix 
+    - m == matrix.length 
+    - n == matrix[i].length
+    - all integers
+    - does not have to be a square
+    - 1 <= m, n <= 1000
+Each row is sorted in non-decreasing order
+    - each row is always increasing 
+    - values in the matrix can be negative
+The first integer of each row is greater than the last integer of the previous row.
+    - first integer of each row === matrix[i][0]
+    - last integer of previous row === matrix[i][n - 1]
+Given an integer target, return true if target is in matrix or false otherwise.
+    - target is given and <= 10000
+
+example 1 
+    (Brute Force)
+    - target given === 3
+    - to check the matrix you would have to iterate through it so,
+    - pick a starting point (left to right)
+    - loop through matrix length 
+    - for each matrix[i] we loop through the nested array matrix[i][j] and see if it matches the target
+    - for each value we have to check if target greater or less than it 
+    - if value is greater than target return false 
+    - else continue through loop
+    How would I try to optimize this ? 
+        - lets say we have a matrix of size 10 x 10
+        - it would be too slow to start in the beginning so where would i start ?
+        - binary search.... time complexity for this is also O(log(m*n))
+    Example 1 with binary search:
+
+
+O(log(m * n))
+*/
+
+
 var searchMatrix = function(matrix, target) {
-    let left = 0;
+    let left = 0
     let right = matrix.length - 1
-    let row = 0;
-    // Use binary search to get the row where our target lies
+    let row = 0
     while (left <= right) {
-        // Get the middle row first to check if target is contained in this row, greater, or less than this row
         const mid = left + Math.floor(right - left / 2)
         if (target < matrix[mid][0]) {
             right = mid - 1
@@ -23,7 +59,6 @@ var searchMatrix = function(matrix, target) {
     if (!(left <= right)){
         return false
     }
-    // Then do another binary search on that specific row to get the target
     right = matrix[row].length - 1
     while (left <= right) {
         const mid = left + Math.floor(right - left / 2)
