9b1203574b70c9ca8c66361b3ff7fb049085589f
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2024-08-26 12:03:56 -0400

    Time: 80 ms (29.13%) | Memory: 52.8 MB (56.76%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -3,27 +3,50 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function(s, t) {
-// Initial approach: iterate through s and t and store number of characters in 
-// a hashmap.
-// Compare hashmap if they are the same then return true, if not, return false.
+/*
+Given two strings s and t
+    
+return true if t is an anagram of s
 
-// OR
-// Since they are all english letters, can initialize an array of all 26 letters
-    if (s.length !== t.length) {
+return false otherwise
+
+word or phrase formed by rearranging the letters of a different word or phrase
+typically using all the original letters exactly once.
+
+s and t consist of lowercase English letters.
+
+Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+
+example 1:
+    s = anagram , t = nagaram
+    first check s string then check t string O(m + n) where m,n is the length of s and t
+    iterate through s and get the counts of each letter store it in a hashmap with the key as the letter and count as the value
+
+    check hashmap if all values are 0 return true, false otherwise
+*/
+var isAnagram = function(s, t) {
+    if (s.length != t.length) {
         return false
     }
-    let chars = new Map()
-
-    for (c of s) {
-        chars.set(c, (chars.get(c) || 0) + 1)
+    let counts = new Map()
+    for (char of s) {
+        if (counts.has(char)) {
+            counts.set(char, counts.get(char) + 1)
+        } else {
+            counts.set(char, 1)
+        }
     }
-    for (c of t) {
-        if (!chars.has(c) || chars.get(c) === 0) {
+    for (char of t) {
+        if (counts.has(char)) {
+            counts.set(char, counts.get(char) - 1)
+        } else {
             return false
         }
-        chars.set(c, (chars.get(c) - 1))
+    }
+    for (val of counts.values()) {
+        if (val != 0) {
+            return false
+        }
     }
     return true
-
 };
