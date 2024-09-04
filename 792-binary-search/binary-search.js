f793f881ba1c433bd1ed94df9dbb5906db8af8d3
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2024-09-04 12:26:58 -0400

    Time: 63 ms (20.64%) | Memory: 52.3 MB (45.45%) - LeetSync

--- 792-binary-search/binary-search.js
+++ 792-binary-search/binary-search.js
@@ -4,25 +4,22 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    // let lo = 0, hi = nums.length-1;
-    // while (lo < hi) {
-    //     let mid = lo + Math.floor((hi-lo+1)/2);
-    //     if (target < nums[mid]) {
-    //         hi = mid - 1
-    //     } else {
-    //         lo = mid; 
-    //     }
-    // }
-    // return nums[lo]==target?lo:-1;
-    let low = 0
-    let high = nums.length - 1
-    while (low < high) {
-        let mid = low + Math.floor( (high - low) / 2)
+    // nums is an array of integers sorted in ascending order
+    // inputs: nums and an integer target
+    // outputs: the index of target or -1
+    // hints: ascending order, and O(logn) = binary search
+
+    let l = 0;
+    let r = nums.length - 1
+    while (l <= r) {
+        const mid = Math.floor((l + r) / 2)
         if (target > nums[mid]) {
-            low = mid + 1;
+            l = mid + 1
+        } else if (target < nums[mid]){
+            r = mid - 1
         } else {
-            high = mid;
+            return mid
         }
     }
-    return nums[low] == target ? low : -1
+    return -1
 };
