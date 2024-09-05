5c73b3d51de8f4fee4040e69f0d58d14f49770a6
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2024-09-04 23:58:15 -0400

    Time: 53 ms (85.61%) | Memory: 52.4 MB (7.17%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.js
+++ 206-reverse-linked-list/reverse-linked-list.js
@@ -10,14 +10,37 @@
  * @return {ListNode}
  */
 var reverseList = function(head) {
+    // traverse the list till the end
+    // while traversing, reverse the nodes 
+
+    // at 1, what are my actions ? 
+    // create a temp node 2 , then set 1.next = prev
+    // at 2 what are my actions
+
+    // iteratively we can use pointers
+    /*
     let prev = null
-    let curr = head
-    while(curr) {
-        const tmp = curr.next
-        curr.next = prev
-        prev = curr
-        curr = tmp
-        
+    let current = head
+    while (current) {
+        const next = current.next
+        current.next = prev
+        prev = current
+        current = next
     }
-    return prev 
+    return prev
+    */
+
+    if (!head) {
+        return null 
+    }
+    let newHead = head
+    if (head.next) {
+        newHead = reverseList(head.next)
+        head.next.next = head
+    }
+    head.next = null 
+    return newHead
+
+
+
 };
