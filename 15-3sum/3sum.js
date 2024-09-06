6f6e148af0c89e9e17d15a09eff489ec3b1e6464
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2024-09-06 11:37:54 -0400

    Time: 156 ms (64.07%) | Memory: 65.9 MB (31.19%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -2,47 +2,50 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
+
+/*
+    inputs: array nums 
+    outputs: array of triplets [nums[i], nums[j], nums[k]] i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+    summarize: given an array of nums we want to find all triplets that sum up to 0
+    constraints: nums length >= 3, nums[i] can be negative, no duplicate triplets
+    order does not matter
+
+    nums = [-1,0,1,2,-1,-4]
+    => [-4,-1,-1,0,1,2]
+    iterate through nums array while checking if it sums to 0 
+    at -1, we check what is it that we need to get this to sum to 0 => 0 - -1 = +1
+    
+
+*/
 var threeSum = function(nums) {
-    // [-4,-1,-1,0,1,2]
-    // Approach: do a while a loop for each element in nums to look for two other numbers whose sum will equal 0
-    // But this makes it so that there is unecessary loooping being done.
-    // How do we get rid of these unecessary steps?
-    // -1 -1 2 -> -1 0 1 -> 0
-    let ans = []
-    if (nums.length < 3) {
-        return ans
-    }
-    nums.sort((a,b) => a - b)
-    let target = 0;
-    // turn this problem in to 2 sum with edge cases
+    let output = []
+    nums.sort((a,b) => (a - b))
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] > 0) {
             break
         }
         if (i > 0 && nums[i] === nums[i - 1]) {
-            continue;
+            continue
         }
         let l = i + 1
         let r = nums.length - 1
-        const partialTarget = target - nums[i]
         while (l < r) {
-            if (nums[l] + nums[r] < partialTarget) {
-                l++
-            } else if (nums[l] + nums[r] > partialTarget) {
-                r--
+            const sum = nums[i] + nums[l] + nums[r]
+            if (sum > 0) {
+                r -= 1
+            } else if (sum < 0) {
+                l += 1
             } else {
-                ans.push([nums[i], nums[l], nums[r]])
+                output.push([nums[i],nums[l],nums[r]])
+                // now move pointers to avoid duplicate iterations
                 const prevL = l
-                const prevR = r
-                l++
-                r--
-                while (nums[l] === nums[prevL] && nums[r] === nums[prevR]) {
+                l++ 
+                while (nums[prevL] == nums[l] && l < r) {
                     l++
-                    r--
                 }
+
             }
         }
     }
-    return ans
-
+    return output
 };
