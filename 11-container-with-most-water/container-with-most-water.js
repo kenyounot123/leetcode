c0f437e273c9bd18c2c70c15e79513c4d56d2ca1
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2024-09-07 22:58:24 -0400

    Time: 75 ms (21.23%) | Memory: 57.7 MB (13.42%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.js
+++ 11-container-with-most-water/container-with-most-water.js
@@ -2,18 +2,35 @@
  * @param {number[]} height
  * @return {number}
  */
+ /*
+ You are given an integer array height of length n
+ two endpoints of the ith line are (i, 0) and (i, height[i]).
+ Find two lines that together with the x-axis form a container, 
+ such that the container contains the most water.
+ Return the maximum amount of water a container can store.
+ inputs: height = nums[]
+ outputs: max integer
+ summarize: find the two lines such taht the container that they form trap
+ the most water. then return the max water contained
+ height = [1,1]
+ amount of water trapped is just 1 
+ this is found by doing (1 - 0) * 1 => i2 - i1 * smaller height
+ height = [1,8,6,2]
+             l r
+ */
 var maxArea = function(height) {
-    let currentMax = 0;
+    maxWater = 0
     let l = 0
     let r = height.length - 1
     while (l < r) {
+        const water = (r - l) * Math.min(height[l], height[r])
+        maxWater = Math.max(water, maxWater)
         if (height[l] < height[r]) {
-            currentMax = Math.max(currentMax, height[l] * (r - l))
             l++
         } else {
-            currentMax = Math.max(currentMax, height[r] * (r - l))
             r--
         }
     }
-    return currentMax
+    
+    return maxWater
 };
