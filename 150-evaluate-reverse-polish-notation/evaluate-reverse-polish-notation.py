d2f13c660483f220a05619cab86326ca7b41cbbc
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2024-09-13 15:29:44 -0400

    Time: 79 ms (5.06%) | Memory: 13.6 MB (50.02%) - LeetSync

---
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -0,0 +1,51 @@
+class Solution(object):
+    def evalRPN(self, tokens):
+        """
+        :type tokens: List[str]
+        :rtype: int
+        """
+        '''
+        You are given an array of strings token
+        Return an integer that represents the value of the expression.
+
+        when you see an operation we need to take the previous to numbers and perform that operation
+        nubmers in tokens array can be negative as well
+        edge cases: token lenght can be 1, in that case we just return the number ?
+
+        tokens = ["2","1","+","3","*"]
+        output = 9
+
+        iterate through tokens
+        at 2, what are my actions ? 
+            this is not an operation ( +-/* ) we can store it in another array [2] and go next
+        at 1 
+            store in to the array [2,1] and go next
+        at +
+            we should take the previous 2 numbers and perform 2 + 1 = 3 
+            pop the 2 numbers and add 3 to the array [3]
+        at 3 
+            [3,3]
+        at * 
+            [9]
+        
+        return stack[0]
+        '''
+        stack = []
+        operations = "*+-/"
+        for num in tokens:
+            if num in operations:
+                num1 = int(stack.pop())
+                num2 = int(stack.pop())
+                if num == '*':
+                    stack.append(num2 * num1)
+                elif num == '+':
+                    stack.append(num2 + num1)
+                elif num == '-':
+                    stack.append(num2 - num1)
+                elif num == '/':
+                    stack.append(int(float(num2) / num1))
+            else:
+                stack.append(num)
+
+        return int(stack[0])
+
