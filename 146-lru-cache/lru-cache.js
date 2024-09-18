a160baac9f87cff2dbc9dd41877bce69fc385340
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2024-09-18 16:39:43 -0400

    Time: 460 ms (87.78%) | Memory: 106.7 MB (27.67%) - LeetSync

--- 146-lru-cache/lru-cache.js
+++ 146-lru-cache/lru-cache.js
@@ -1,22 +1,11 @@
-class Node {
-    constructor(key, value) {
-        this.key = key
-        this.val = value
-        this.prev = null
-        this.next = null
-    }
-}
+
 /**
  * @param {number} capacity
  */
 class LRUCache {
     constructor(capacity) {
         this.capacity = capacity
-        this.memory = new Map() // Maps key to node
-        this.leastRecent = new Node(0,0)
-        this.mostRecent = new Node(0,0)
-        this.leastRecent.next = this.mostRecent // doubly linked list
-        this.mostRecent.prev = this.leastRecent 
+        this.memory = new Map()
     }
     
 };
@@ -26,13 +15,13 @@ class LRUCache {
  * @return {number}
  */
 LRUCache.prototype.get = function(key) {
-    if (this.memory.has(key)) {
-        this.remove(this.memory.get(key)) // Remove this key 
-        this.insert(this.memory.get(key)) // Update position of this key to be most recent
-        return this.memory.get(key).val
-    } 
-    return -1
-    
+    if (!this.memory.has(key)) {
+        return -1
+    }
+    const value = this.memory.get(key)
+    this.memory.delete(key)
+    this.memory.set(key, value)
+    return this.memory.get(key)
     
 };
 
@@ -43,31 +32,15 @@ LRUCache.prototype.get = function(key) {
  */
 LRUCache.prototype.put = function(key, value) {
     if (this.memory.has(key)) {
-        this.remove(this.memory.get(key))
+        this.memory.delete(key)
     }
-    this.memory.set(key, new Node(key, value)) 
-    this.insert(this.memory.get(key))
+    this.memory.set(key, value)
 
     if (this.memory.size > this.capacity) {
-        const leastRecent = this.leastRecent.next
-        this.remove(leastRecent)
-        this.memory.delete(leastRecent.key)
-    }
+        this.memory.delete(this.memory.keys().next().value)
+    } 
 };
-LRUCache.prototype.remove = function(node) {
-    const prev = node.prev
-    const nxt = node.next
-    prev.next = nxt
-    nxt.prev = prev
-}
-LRUCache.prototype.insert = function(node) {
-    let prev = this.mostRecent.prev
-    let next = this.mostRecent
-    prev.next = node 
-    next.prev = node
-    node.next = next
-    node.prev = prev
-}
+
 /** 
  * Your LRUCache object will be instantiated and called as such:
  * var obj = new LRUCache(capacity)
