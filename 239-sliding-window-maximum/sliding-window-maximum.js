2dfe577656c40d3112c9cff70989e0107216f49f
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2024-09-18 23:10:23 -0400

    Time: 666 ms (20.74%) | Memory: 82.5 MB (27.16%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.js
+++ 239-sliding-window-maximum/sliding-window-maximum.js
@@ -7,36 +7,39 @@
 /*
 inputs: nums:number[], k:number
 output: number[]
-summarize: given a nums array and a window size of k 
-we need to find the maximum value of each window when shifted to the right, then append it 
-to the output array which is an array of all the maxes.
-[1,3,-1]
+to summarize: we are given an array nums and an integer k which represents our max
+window length
+while sliding this max window length across our array we continue to take the max 
+of each window and return an output array that contains all these maxes
+
 */
 var maxSlidingWindow = function(nums, k) {
-    let deque = []
     let output = []
+    let deque = []
     let l = 0
     let r = 0
-    if (nums.length === 1) {
-        return nums
-    }
+    // [1, -1]
+    //.     ^
+    //      ^
+    //  deque = [0,1]
+    // output = [1]
     while (r < nums.length) {
-        // while there are values smaller than current value we can pop it off the deque
-        while ((deque) && (nums[r] > nums[deque[deque.length - 1]])) {
+        while (deque && nums[r] > nums[deque[deque.length - 1]]) {
             deque.pop()
         }
-        // only then can we push it on to our decreasing queue
         deque.push(r)
-        
+
         if (l > deque[0]) {
             deque.shift()
         }
-        if ((r - l + 1) >= k) {
+
+        if ((r - l) + 1 >= k) {
             output.push(nums[deque[0]])
             l++
         }
         r++
     }
+
     return output
 };
 // q = [1]
