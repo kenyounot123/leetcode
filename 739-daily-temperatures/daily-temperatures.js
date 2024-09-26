a1ddbc4fe9573b8ba4920fce96f4af8f48cb524d
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2024-09-26 00:05:11 -0400

    Time: 215 ms (54.37%) | Memory: 71.8 MB (87.49%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.js
+++ 739-daily-temperatures/daily-temperatures.js
@@ -3,39 +3,24 @@
  * @return {number[]}
  */
 var dailyTemperatures = function(temperatures) {
-    let answer = new Array(temperatures.length).fill(0);
-    let stack = [];
+    // inputs: temperatures nums[]
+    // outputs: answer[] number of days you have to wait after ith day to get a warmer temperature
+    // summarize: given an array of integers, for each index return the number (answer[i]) of days 
+    // until we get a higher temperature
 
-    // we store the index of the temperature in the stack
-    // that way we can calculate the # of days till we get a warmer temp by doing index - index of stack
+    // temperatures = [73,74,75,71,69,72,76,73]
+    // so on 73 we need to push 1 to answer []
+    // 74 we need to push 1 to answer
+    // 75 we need to put 4
+    
+    let answer = new Array(temperatures.length).fill(0)
+    let stack = []
     for (let i = 0; i < temperatures.length; i++) {
-        // If stack is not empty and our temp is greater than the previous temp 
         while (stack.length > 0 && temperatures[i] > temperatures[stack[stack.length - 1]]) {
-            // Get previous temp index
-            const index = stack.pop()
-            // calculate # of days till we saw a warmer temp 
-            answer[index] = i - index
+            const prevIndex = stack.pop()
+            answer[prevIndex] = i - prevIndex
         }
         stack.push(i)
     }
     return answer
 };
-    //O(n^2) BAD TOO SLOW
-    // for (let i = 0; i < temperatures.length; i++) {
-    //     let r = i;
-    //     stack.length = 0;
-    //     stack.push(temperatures[i])
-    //     // For each index we want to loop till we find a number > than current index
-    //     while (!(temperatures[r] > stack[0])) {
-    //         r += 1
-    //         if (r > temperatures.length) {
-    //             answer.push(0) 
-    //             break
-    //         }
-    //         if (temperatures[r] > stack[0]) {
-    //             answer.push(stack.length)
-    //         } else {
-    //             stack.push(temperatures[r])
-    //         }
-    //     }
-    // }
