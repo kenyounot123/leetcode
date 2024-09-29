dc9ee743c8480c5f6fb068ba7050789d0348e14a
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2024-09-28 23:44:52 -0400

    Time: 49 ms (94.97%) | Memory: 51.3 MB (33.85%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,21 +4,17 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    // summarize: given array of integers nums and a target, find the two numbers that sum up to target
-    // guaranteed 1 valid answer
-    // output is index
-    // less than O(n^2)
-    // nums.length >= 2
-    // iterate through nums [2,7,11,15] 
-    // store 2 in {2:0} 
+    // inputs: nums[] and target(number)
+    // outputs: [] containts indexes
+    // nums = [3,2,4] target = 6
+    // { 3 : 0, 2 : 1,  }
     let hm = new Map()
-    let result = []
-    for (let i = 0; i <= nums.length; i++) {
-        const difference = target - nums[i]
-        if (hm.has(difference)) {
-            return [i, hm.get(difference)]
-        } else {
-            hm.set(nums[i], i)
+    for (let i = 0; i < nums.length; i++) {
+        if (hm.has(target - nums[i])) {
+            return [hm.get(target - nums[i]), i]
         }
+        hm.set(nums[i], i)
     }
+
+
 };
