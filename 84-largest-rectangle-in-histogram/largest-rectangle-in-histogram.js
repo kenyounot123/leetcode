039cab1046e10b23017a7c0bd533cb48e5c0d08a
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2024-09-28 21:00:53 -0400

    Time: 151 ms (21.09%) | Memory: 76.6 MB (23.85%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.js
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.js
@@ -3,20 +3,23 @@
  * @return {number}
  */
 var largestRectangleArea = function(heights) {
-    let stack = []; //pairs of index and height
-    let maxArea = 0;
+    // integer heights []
+    // inputs: heights[] heights[i] is how tall it is , width is always 1
+    // outputs: return largest rectangle ( area of rectangle )
+    let maxArea = 0
+    let stack = []
+    heights.push(0)
     for (let i = 0; i < heights.length; i++) {
-        let start = i;
-        // When current height is less than prev height
-        while (stack.length !== 0 && stack[stack.length - 1][1] > heights[i]) {
-            const [index, height] = stack.pop()
-            maxArea = Math.max(maxArea, (height * (i - index)))
-            start = index
+        let heightStart = i
+        while (stack.length > 0 && stack[stack.length - 1][1] > heights[i]) {
+            const [pos, height] = stack.pop()
+            maxArea = Math.max(maxArea, height * (i - pos))
+            heightStart = pos
         }
-        stack.push([start, heights[i]])
+        stack.push([heightStart, heights[i]])
+
     }
-    for (let j = 0; j < stack.length; j++) {
-        maxArea = Math.max(maxArea, stack[j][1] * (heights.length - stack[j][0]))
-    }
-    return maxArea
+
+    return maxArea 
 };
+// [[1,1] , [2,2]]
