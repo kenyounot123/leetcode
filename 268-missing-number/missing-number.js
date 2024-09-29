7def845153b2ede13c8373339cac380c4eb43376
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2024-09-28 22:32:20 -0400

    Time: 54 ms (78.38%) | Memory: 51.5 MB (45.96%) - LeetSync

--- 268-missing-number/missing-number.js
+++ 268-missing-number/missing-number.js
@@ -4,13 +4,14 @@
  */
 var missingNumber = function(nums) {
     // inputs: nums[]
-    // output: a number that is missing from the array
-    for (let i = 0; i <= nums.length; i++) {
-        if (nums.includes(i)) {
-            continue
-        } else 
-        return i
+    // output: a number that is missing from the array 
+    // n ( n + 1 ) / 2
+    const size = nums.length
+    let sum = 0
+    for (let num of nums) {
+        sum += num
     }
+    return ((size * (size + 1)) / 2) - sum
 
 
 };
