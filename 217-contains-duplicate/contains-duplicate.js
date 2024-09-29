5f2e6ca13c704877c51a022156aebdc9feabdda1
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2024-09-28 21:53:15 -0400

    Time: 87 ms (32.26%) | Memory: 68.9 MB (5.62%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -2,37 +2,16 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-
-// Given an integer array nums
-//     - int[]
-//     - length of nums is small
-//     - values can be negative or positive
-// return true if any value appears at least twice in the array
-
-// return false if every element is distinct
-
-// Example 1:
-//     [1,2,3,1]
-//     start iterating the array :
-//         1 ,2 ,3 ,1
-//         - 1 ( is this a duplicate ? ) no 
-//             - 2 ( is this a duplicate ?) no 
-//                 -3 ( is this a duplicate ?) no
-//                     -1 (is this a duplicate ?) YES
-
-//         return true
-// TC:
-//     O(n) n = number of items in array num[]
-// SC:
-//     O(1) 
-
 var containsDuplicate = function(nums) {
-    const hm = new Map()
-    for (num of nums) {
-        if (hm.has(num)) {
+    // Given an integer array nums, return true if any value appears at least twice 
+    // false if every element is distinct.
+    let hm = new Map()
+    for (let num of nums) {
+        hm.set(num, (hm.get(num) || 0) + 1)
+    }
+    for (let val of hm.values()) {
+        if (val > 1) {
             return true
-        } else {
-            hm.set(num, 1)
         }
     }
     return false
