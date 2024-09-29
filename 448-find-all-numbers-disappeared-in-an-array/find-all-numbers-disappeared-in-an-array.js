1ac6090c336880feb8306e9363de9a1f35c58191
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2024-09-28 23:32:35 -0400

    Time: 90 ms (51.82%) | Memory: 58.6 MB (71.82%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.js
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.js
@@ -3,13 +3,31 @@
  * @return {number[]}
  */
 var findDisappearedNumbers = function(nums) {
-
-    let set = new Set(nums) // O(n)
+    // O(n) space
+    // let set = new Set(nums)
     let outputs = []
-    for (let i = 1; i <= nums.length; i++) {
-        if (!set.has(i)){
-            outputs.push(i)
+    // for (let i = 1; i <= nums.length; i++) {
+    //     if (!set.has(i)){
+    //         outputs.push(i)
+    //     }
+    // }
+    // [2,1,2,3]
+    // 2 - 1 = 1
+    // [-2,1,2,3]
+    // O(1)
+    for (let num of nums) {
+        const index = Math.abs(num) - 1
+
+        if (nums[index] > 0) {
+            nums[index] = nums[index] * -1
         }
+    }
+    
+    for (let i = 0; i < nums.length; i++){
+        if (nums[i] > 0) {
+            outputs.push(i + 1)
+        }
+
     }
 
 
