953b23779e55be7a4caede7a6af04f1db302a77d
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2024-10-01 23:52:30 -0400

    Time: 59 ms (88.38%) | Memory: 53.2 MB (19.84%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.js
+++ 907-koko-eating-bananas/koko-eating-bananas.js
@@ -3,32 +3,38 @@
  * @param {number} h
  * @return {number}
  */
+// There are n piles of bananas, the ith pile has piles[i] bananas
+// The guards have gone and will come back in h hours.
+// Each hour, she chooses some pile of bananas and eats k bananas from that pile.
+// If the pile has less than k bananas, she eats all of them instead and will not 
+// eat any more bananas during this hour.
+// Return the minimum integer k such that she can eat all the bananas within h hours.
+// summarize: we basically need to pick a k where k is the number of bananas koko will eat
+// pick a minimum k such that in h hours, koko will eat all the bananas.
+// 8 hours to finish 27 bananas
+// 5 hrs to finish 88 bananas ( 18 )
+// problem with this is that we cant move on to the next pile unless we finish the first pile
+// [4,11,20,23,30]
 var minEatingSpeed = function(piles, h) {
-    // [3,6,7,11]
-    // Every hr, can only eat up to 1 pile max
-    //  piles.length <= h
-    // Intuition: We can try eating bananas with k = 1,2,3... all the way up to our max k which is just
-    // the largest value in piles. 
-    // So looping through our piles array, suppose k = 1, and piles = [3,6,7,11]
-    // it will take 3 + 6 + 7 + 11 hrs to finish eating all bananas and that is not <= h so k cannot be 1
-    // Can repeat this all the way up to 11 till we find the first instance of a valid k.
-    // OR since we know k = [1,2,3,4,5,6,7,8,9,10,11] we can do a binary search on k.
-    let l = 1;
-    let r = Math.max(...piles);
-    let minK = r;
-    const hoursTaken = (speed) => {
-        return piles.reduce((sum, pile) => sum + Math.ceil(pile / speed), 0)
+    const checkHrs = (piles, k) => {
+        let hrs = 0
+        for (let p of piles) {
+            hrs += Math.ceil(p / k)
+        }
+        return hrs
     }
+    let l = 1
+    let r = Math.max(...piles)
+    let result = 0
     while (l <= r) {
-        const mid = Math.floor((r + l) / 2)
-        
-        if (hoursTaken(mid) <= h) {
-            minK = mid
+        const mid = Math.floor((l + r) / 2)
+        const hours = checkHrs(piles, mid)
+        if (hours <= h) {
+            result = mid
             r = mid - 1
         } else {
             l = mid + 1
         }
     }
-    return minK
-
+    return result
 };
