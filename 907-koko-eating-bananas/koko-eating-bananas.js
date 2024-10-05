000506fff584c3fcbb7ab17ef5fcce3b4e03118a
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2024-10-04 23:00:53 -0400

    Time: 70 ms (44.49%) | Memory: 52.9 MB (28.01%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.js
+++ 907-koko-eating-bananas/koko-eating-bananas.js
@@ -3,38 +3,43 @@
  * @param {number} h
  * @return {number}
  */
-// There are n piles of bananas, the ith pile has piles[i] bananas
-// The guards have gone and will come back in h hours.
-// Each hour, she chooses some pile of bananas and eats k bananas from that pile.
-// If the pile has less than k bananas, she eats all of them instead and will not 
-// eat any more bananas during this hour.
-// Return the minimum integer k such that she can eat all the bananas within h hours.
-// summarize: we basically need to pick a k where k is the number of bananas koko will eat
-// pick a minimum k such that in h hours, koko will eat all the bananas.
-// 8 hours to finish 27 bananas
-// 5 hrs to finish 88 bananas ( 18 )
-// problem with this is that we cant move on to the next pile unless we finish the first pile
-// [4,11,20,23,30]
 var minEatingSpeed = function(piles, h) {
-    const checkHrs = (piles, k) => {
-        let hrs = 0
-        for (let p of piles) {
-            hrs += Math.ceil(p / k)
+    // given an array of nums and a number as inputs
+    // outputs: number
+    // summarize: we are given an array of nums and some time h
+    // we want to find k where k is the minimum number where we can eat all piles within h hours
+
+    // if k > piles[i] then we eat a pile => remaining pile length - 1
+    // the most we can eat is max(piles) since it would mean we eat 1 pile every hour
+    // if eat too slow > h => then up the speed (k)
+    // if eat too fast < h => then lower the speed (k)
+
+    // check from k = 1  to  max(piles) [1,2,3,4,5,6,7,8,9,10,11]
+
+
+    const checkSpeed = (k, h, piles) => {
+        let speed = 0
+        for (let num of piles) {
+            speed += Math.ceil(num / k)
         }
-        return hrs
+        return speed
     }
+
     let l = 1
     let r = Math.max(...piles)
-    let result = 0
+    let minK = 0
+
     while (l <= r) {
         const mid = Math.floor((l + r) / 2)
-        const hours = checkHrs(piles, mid)
-        if (hours <= h) {
-            result = mid
+        const speed = checkSpeed(mid, h, piles)
+        if (speed <= h) {
+            minK = mid
             r = mid - 1
         } else {
             l = mid + 1
         }
-    }
-    return result
+    } 
+
+    return minK
+
 };
