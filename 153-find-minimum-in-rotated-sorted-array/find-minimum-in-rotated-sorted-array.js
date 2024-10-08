21f856536467e4da6361902fb25a05a90456a47b
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2024-10-07 23:20:35 -0400

    Time: 57 ms (26.83%) | Memory: 49.4 MB (6.84%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
@@ -3,15 +3,31 @@
  * @return {number}
  */
 var findMin = function(nums) {
+    // input: nums array which is sorted in ascending order but rotated
+    // outputs: return a number which is the minimum element
+    // summarize: given an array of nums that is already sorted but rotated some amount 
+    // of times, we need to find the minimum element of this array.
+    // if it was just a normal sorted array , we can just return nums[0], but the first
+    // index in this rotated array is not always going to be the position of the 
+    // smallest element
     let l = 0
     let r = nums.length - 1
-    while (l < r) {
-        const mid = l + Math.floor((r - l) / 2)
-        if (nums[mid] > nums[r]) {
+    // nums[l] > nums[r]
+    // [4,0,1,2,3]
+    let min = 9999
+    while (l <= r) {
+        const mid = Math.floor((l + r) / 2)
+        if (nums[l] <= nums[r]) {
+            min = Math.min(min, nums[l])
+            break
+        }
+        if (nums[mid] >= nums[l]) {
+            min = Math.min(min, nums[l])
             l = mid + 1
         } else {
-            r = mid  
+            min = Math.min(min, nums[mid])
+            r = mid - 1
         }
     }
-    return nums[l]
+    return min
 };
