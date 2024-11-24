d68cabddc1aa5fc5959f3a9d10cc9ead9feabb1e
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2024-11-23 22:56:04 -0500

    Time: 0 ms (100.00%) | Memory: 51 MB (29.85%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.js
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.js
@@ -3,14 +3,11 @@
  * @return {boolean}
  */
 var uniqueOccurrences = function(arr) {
-    // inputs:given an array of nums 
-    // outputs: a boolean returm true if each value's number of occurrences is unique return false otherwise
-    let occurrences = new Map()
-    for (let num of arr) {
-        occurrences.set(num, (occurrences.get(num) || 0) + 1)
+    let counts = new Map()
+    for (const num of arr) {
+        counts.set(num, (counts.get(num) || 0) + 1)
     }
 
-    let uniq = new Set(occurrences.values())
-    return uniq.size === occurrences.size
-
+    let uniqCounts = new Set(counts.values())
+    return counts.size === uniqCounts.size
 };
