ebcb107b23b4d4051711567afee896607729c22b
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2025-01-06 22:24:56 -0500

    Time: 0 ms (100.00%) | Memory: 211.7 MB (54.90%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.rb
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.rb
@@ -7,12 +7,6 @@ def gcd_of_strings(str1, str2)
     a = str1.length
     b = str2.length
 
-    while b != 0 do
-        tmp = a % b
-        break if tmp == 0
-        a = b
-        b = tmp
-    end
-
-    return str1[0,b]
+    gcd = a.gcd(b)
+    str1[0...gcd]
 end
