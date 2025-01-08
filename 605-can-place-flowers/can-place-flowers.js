6958cb474be52c494bdf1a917328fedae2aa4ef7
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2025-01-07 22:19:35 -0500

    Time: 1 ms (86.14%) | Memory: 51 MB (58.29%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.js
+++ 605-can-place-flowers/can-place-flowers.js
@@ -4,16 +4,18 @@
  * @return {boolean}
  */
 var canPlaceFlowers = function(flowerbed, n) {
-    if (!flowerbed.length) {
-        return false
-    }
-    // [1,0,0,0,0,1]
-     for (let i = 0; i < flowerbed.length; i++) {
-        if (flowerbed[i] === 0 && !flowerbed[i - 1] && !flowerbed[i + 1]) {
-            i++;
-            n--
+    
+    for (let i = 0; i < flowerbed.length; i++) {
+        let j = i
+        if (flowerbed[i] === 0 && 
+            (i === 0 || flowerbed[i - 1] === 0) &&
+            (i === flowerbed.length - 1 || flowerbed[j + 1] === 0)) {
+            flowerbed[i] = 1
+            n -= 1
+        }
+        if (n <= 0) {
+            return true;
         }
     }
     return n <= 0
-
 };
