fb6bc5cd67e5d7863a1c6aefdb6f3004c0afae25
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2025-01-08 21:46:08 -0500

    Time: 252 ms (5.05%) | Memory: 53.7 MB (94.43%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.js
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.js
@@ -3,24 +3,25 @@
  * @return {string}
  */
 var reverseVowels = function(s) {
-    const vowels = "aeiouAEIOU"
-    let allVowelsInString = []
-    const strArray = s.split('')
+    const vowels = 'aeiouAEIOU'
+    let vowelsInString = []
+    let output = ""
+
     for (let i = 0; i < s.length; i++) {
         if (vowels.includes(s[i])) {
-            allVowelsInString.push([s[i], i])
-        } 
+            vowelsInString.push(s[i])
+        }
     }
 
-    let l = 0
-    let r = allVowelsInString.length - 1
-    while (l < r) {
-        const tmp = strArray[allVowelsInString[l][1]]
-        strArray[allVowelsInString[l][1]] = strArray[allVowelsInString[r][1]]
-        strArray[allVowelsInString[r][1]] = tmp
-        l++
-        r--
+    vowelsInString.reverse()
+
+    for (let i = 0; i < s.length; i++) {
+        if (vowels.includes(s[i])) {
+            output += vowelsInString.shift()
+        } else {
+            output += s[i]
+        }
     }
 
-    return strArray.join("")
+    return output
 };
