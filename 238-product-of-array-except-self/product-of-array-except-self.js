fb84696d71c74e3f530cc4bf0393a2975948eb22
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2025-01-09 23:49:44 -0500

    Time: 9 ms (46.84%) | Memory: 66.5 MB (24.64%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.js
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -4,21 +4,25 @@
  */
 var productExceptSelf = function(nums) {
     // nums = [1,2,3,4]
-    // answer = [1,1,2,6]
-
-    // want = [24,12,8,6]
-    let answer = new Array(nums.length).fill(1)
-    for (let i = 1; i < nums.length; i++) {
-          answer[i] = answer[i - 1] * nums[i - 1];
+    // [1, 1, 2, 6]
+    let output = new Array(nums.length).fill(1)
+    for (let i = 0; i < nums.length; i++) {
+        if (i === 0) { // left boundary product should be 1 because nothing on the left
+            continue
+        }
+        output[i] = output[i - 1] * nums[i - 1]
     }
-    let right = nums[nums.length - 1]
-    for (let i = nums.length - 2; i >= 0; i--) {
-        answer[i] = answer[i] * right
-        right *= nums[i]
+
+    let rightProd = nums[nums.length - 1]
+
+    for (let i = nums.length - 1; i >= 0; i--) {
+        if (i === nums.length - 1) {
+            continue
+        }
+        output[i] = output[i] * rightProd
+        rightProd *= nums[i]
     }
-    
 
+    return output
 
-
-    return answer
-};
+};  
