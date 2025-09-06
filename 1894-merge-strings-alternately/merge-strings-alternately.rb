6d6f7338b9b73a96fdbdfed42cb021d89d001ae8
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2025-09-06 16:27:21 -0400

    Time: 71 ms (23.35%) | Memory: 211.5 MB (94.42%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.rb
+++ 1894-merge-strings-alternately/merge-strings-alternately.rb
@@ -4,11 +4,11 @@
 def merge_alternately(word1, word2)
     # Inputs: word1 and word2, strings
     # output: 1 merged string
-    result = []
-    longest_word_length = word1.size >= word2.size ? word1.size : word2.size
+    result = ""
+    longest_word_length = [word1.size, word2.size].max
     (0..longest_word_length).each do |i|
         result << word1[i] if word1[i]
         result << word2[i] if word2[i]
     end
-    result.join("")
+    result
 end
