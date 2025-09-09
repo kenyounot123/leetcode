524e9488e3bfd3e845718357ee776cc785ad8960
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2025-09-08 20:55:47 -0400

    Time: 47 ms (73.84%) | Memory: 221.3 MB (28.07%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.rb
@@ -0,0 +1,22 @@
+# @param {Integer[]} prices
+# @return {Integer}
+def max_profit(prices)
+    # Input: prices[] nums
+    # Output: will be either 0 or profit
+    # If prices is sorted and is in descending order, then we return 0
+    profit = 0
+    total = 0
+    l = 0
+    r = 0
+    while l <= r && r < prices.length
+        if prices[r] <= prices[l]
+            l = r
+            r += 1
+            next
+        else
+            profit = [profit, prices[r] - prices[l]].max
+        end
+        r += 1
+    end
+    profit
+end
