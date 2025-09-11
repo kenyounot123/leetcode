2ffe7e52769ba8bf0f884258bd579e6813d283d7
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2025-09-11 19:23:57 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (30.53%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -3,18 +3,20 @@ class Solution:
         # inputs: strs[] list of strs
         # outputs: the longest common prefix or "" if none
         
-        compare_s = strs[0]
-        
-        longest_pref_len = min(len(s) for s in strs)
+        # array can never be empty
 
-        for i in range(longest_pref_len):
-            char = compare_s[i]
-            for j in range(1,len(strs)):
-                if strs[j][i] == char:
+        compare_s = strs[0]
+
+        max_pref_len = min(len(s) for s in strs)
+
+        for i in range(max_pref_len):
+            pref = compare_s[i]
+
+            for j in range(1, len(strs)):
+                if pref == strs[j][i]:
                     continue
                 else:
-                    return compare_s[:i]
-        return compare_s[:longest_pref_len]
+                    return compare_s[0:i]
 
-        
+        return compare_s[:max_pref_len]
 
