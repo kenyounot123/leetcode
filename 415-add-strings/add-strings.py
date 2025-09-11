04b3eba6a897efa162cd9023040b0ce71d6222bd
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2025-09-11 18:29:24 -0400

    Time: 7 ms (44.96%) | Memory: 18.1 MB (54.64%) - LeetSync

--- 415-add-strings/add-strings.py
+++ 415-add-strings/add-strings.py
@@ -3,20 +3,28 @@ class Solution:
         # inputs: 2 strings 
         # outputs: sum of those 2 strings as a string
         # no string to int conversion
-        
-        i, j = len(num1) - 1, len(num2) - 1
+        # convert string to ascii to get an integer val
+        ans = ""
         carry = 0
-        result = []
-
-        while i >= 0 or j >= 0 or carry:
-            digit1 = ord(num1[i]) - ord("0") if i >= 0 else 0
-            digit2 = ord(num2[j]) - ord("0") if j >= 0 else 0
-
-            total = digit1 + digit2 + carry
+        l = 0
+        r = 0
+        r1 = num1[::-1]
+        r2 = num2[::-1]
+        # "321"
+        # "11"
+        while l < len(num1) or r < len(num2) or carry:
+            d1 = ord(r1[l]) - ord("0") if l < len(num1) else 0
+            d2 = ord(r2[r]) - ord("0") if r < len(num2) else 0
+            total = d1 + d2 + carry
             carry = total // 10
-            result.append(str(total % 10))
+            remainder = total % 10
 
-            i -= 1
-            j -= 1
-        return "".join(result[::-1])
+            ans += str(remainder)
+            l += 1
+            r += 1
+
         
+
+        return ans[::-1]
+
+
