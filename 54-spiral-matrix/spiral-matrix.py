5a666712a95f19e0427f5dc8a3ab28a623c3b5bb
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2025-09-14 15:31:14 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (18.64%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -5,8 +5,9 @@ class Solution:
         output = []
         top, bottom = 0, len(matrix) - 1
         left, right = 0, len(matrix[0]) - 1
+
         while top <= bottom and left <= right:
-            for i in range(left,right + 1):
+            for i in range(left, right + 1):
                 output.append(matrix[top][i])
             top += 1
 
@@ -18,10 +19,9 @@ class Solution:
                 for i in range(right, left - 1, -1):
                     output.append(matrix[bottom][i])
                 bottom -= 1
-
             if left <= right:
                 for i in range(bottom, top - 1, -1):
                     output.append(matrix[i][left])
                 left += 1
-        
+
         return output
