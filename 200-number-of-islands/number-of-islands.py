fc71f17a3ecf336c35957899efff5a770eea3c57
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2025-09-14 22:29:53 -0400

    Time: 319 ms (8.99%) | Memory: 25.3 MB (29.94%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,48 +1,39 @@
-class Solution(object):
-    def numIslands(self, grid):
-        """
-        :type grid: List[List[str]]
-        :rtype: int
-        """
-        '''
-        inputs: m x n grid of 0s and 1s
-        outputs: integer that represents the amount of islands 
-        m is rows
-        n is columns
-        edge cases: what if theres only 1 row can there be an island?
-        this should not affect it because we assume all edges are surrounded by water
-        iterate through grid:
-            decision:
-                we need to perform some sort of check if we hit a 1
-                if we hit a 0 we can just go to the next value
-            
-        '''
-        rows, cols = len(grid), len(grid[0])
-        islands = 0
-        visited = set()
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
         
+        rows, cols = len(grid), len(grid[0])
+        visited = set()
+        num_islands = 0
 
-        def bfs(r, c):
-            # check adjacent positions to see if our island is connected to other 1s
+        def bfs(r,c):
             q = collections.deque()
             visited.add((r,c))
             q.append((r,c))
-            directions = [[0, 1], [-1, 0], [0, -1], [1, 0]]
 
             while q:
                 row, col = q.popleft()
+                directions = [[1,0],[-1,0],[0,1], [0,-1]]
                 for x,y in directions:
                     r, c = row + x, col + y
                     if (r in range(rows) and 
-                       c in range(cols) and 
-                       grid[r][c] == "1" and
-                       (r,c) not in visited):
-                       q.append((r,c))
-                       visited.add((r,c))
+                        c in range(cols) and
+                        grid[r][c] == "1" and
+                        (r,c) not in visited):
+                        q.append((r,c))
+                        visited.add((r,c))
+                        
+
 
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == '1' and (r,c) not in visited:
+                if grid[r][c] == "1" and (r,c) not in visited:
                     bfs(r,c)
-                    islands += 1
-        return islands
+                    num_islands += 1
+
+
+
+
+        return num_islands
+        
