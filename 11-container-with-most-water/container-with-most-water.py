e2e73e6782d5049be9bdbe2a8b1f823708d21fad
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2025-09-18 19:53:29 -0400

    Time: 70 ms (89.31%) | Memory: 28.4 MB (90.60%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,37 +1,22 @@
-class Solution(object):
-    def maxArea(self, height):
-        """
-        :type height: List[int]
-        :rtype: int
-        """
-        '''
-        given an integer array height of length n
-        n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i])
-        Find two lines that together with the x-axis form a container
-        Return the maximum amount of water a container can store.
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        # inputs: height:int[]
+        # output: find max amount of water int
 
-        summarize: given an array of nums Height return two lines such that they trap the most water, in other words
-        the area of the container formed by these two lines is the max area
-
-        area is given by length x width => in this case width is two indexes i1, i2 and height is height[i]
-
-        we can start from the two end points
-        height = [1,8,6,2,5,4,8,3,7]
-        at index 0 and index 8
-        compare the two heights because a container is formed only by taking area of the smaller height
-
-        '''
+        # we can do 2pointers with greedy approach
+        max_area = 0
         l = 0
         r = len(height) - 1
-        max_area = 0
         while l < r:
-            area = (r - l) * min(height[l], height[r])
-            max_area = max(area, max_area)
-            if height[l] < height[r]:
+            width = (r - l)
+            if height[l] <= height[r]:
+                area = width * height[l]
+                max_area = max(max_area, area)
                 l += 1
             else:
+                area = width * height[r]
+                max_area = max(max_area, area)
                 r -= 1
+        
 
         return max_area
-
-        
