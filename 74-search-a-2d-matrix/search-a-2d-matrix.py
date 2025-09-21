b3cdff3874dd154d6434bdb83c44931f072663ed
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2025-09-20 22:32:52 -0400

    Time: 0 ms (100.00%) | Memory: 18.2 MB (45.49%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,47 +1,27 @@
-class Solution(object):
-    def searchMatrix(self, matrix, target):
-        """
-        :type matrix: List[List[int]]
-        :type target: int
-        :rtype: bool
-        """
-        '''
-        inputs: m x n matrix and an integer target
-        output: boolean 
-        summarize: we are given a mxn matrix where each row 
-        is ascending order and the first integer of each row
-        is greater tahn the last integer of previous row. Given a target,
-        return true if this target is in the matrix, false otherwise
-
-        m == matrix.length
-        n == matrix[i].length
-        O(log(m * n)) => binary search ?
-
-        brute force is to iterate through each nested array O(m*n)
-        '''
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         l = 0
         r = len(matrix) - 1
-        # This finds which nested array target is in
+
         while l < r:
             mid = (l + r) // 2
-            if (target > matrix[mid][len(matrix[mid]) - 1]):
+            row = matrix[mid]
+            if target > row[len(matrix[mid]) - 1]:
                 l = mid + 1
             else:
                 r = mid
         
-        targetRow = matrix[l]
+        target_row = matrix[l]
         l = 0
-        r = len(targetRow) - 1
+        r = len(target_row) - 1
+
         while l < r:
             mid = (l + r) // 2
-            if target > targetRow[mid]:
+            if target > target_row[mid]:
                 l = mid + 1
             else:
                 r = mid
-        return True if targetRow[l] == target else False
+        return True if target_row[l] == target else False
 
         
 
-
-
-        
