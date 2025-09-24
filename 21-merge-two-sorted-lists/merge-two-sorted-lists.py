75a4d5ebde1ac64bfb35f1ec65b492825a1ccb96
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2025-09-24 19:33:37 -0400

    Time: 3 ms (12.01%) | Memory: 17.6 MB (88.60%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -5,14 +5,16 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        # Inputs: 2 linked lists
-        # Outputs: 1 linked list that has been formed by merging the two and sorted
+        # inputs: two sorted linked lists l1 and l2
+        # outputs: head of merged sorted linked list
 
-        if list1 is None:
-            return list2
-        if list2 is None:
+        if list1 is None and list2 is None:
             return list1
-
+        if list1 is None and list2 is not None:
+            return list2
+        if list1 is not None and list2 is None:
+            return list1
+        
         if list1.val <= list2.val:
             list1.next = self.mergeTwoLists(list1.next, list2)
             return list1
