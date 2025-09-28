e11c6b09785b9108237d446b6d8fb6d25a6e1322
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2025-09-28 10:31:55 -0400

    Time: 3 ms (63.90%) | Memory: 19.1 MB (51.84%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -1,52 +1,31 @@
-class Solution(object):
-    def evalRPN(self, tokens):
-        """
-        :type tokens: List[str]
-        :rtype: int
-        """
-        '''
-        You are given an array of strings token
-        Return an integer that represents the value of the expression.
-
-        when you see an operation we need to take the previous to numbers and perform that operation
-        nubmers in tokens array can be negative as well
-        edge cases: token lenght can be 1, in that case we just return the number ?
-
-        tokens = ["2","1","+","3","*"]
-        output = 9
-
-        iterate through tokens
-        at 2, what are my actions ? 
-            this is not an operation ( +-/* ) we can store it in another array [2] and go next
-        at 1 
-            store in to the array [2,1] and go next
-        at +
-            we should take the previous 2 numbers and perform 2 + 1 = 3 
-            pop the 2 numbers and add 3 to the array [3]
-        at 3 
-            [3,3]
-        at * 
-            [9]
-        
-        iterating through tokens so O(n) where n is tokens length
-        return stack[0]
-        '''
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        # array of strings tokens[str]
+        # Evaluate it and return an integer
+        valid_operators = "+-*/"
         stack = []
-        operations = "*+-/"
-        for num in tokens:
-            if num in operations:
-                num1 = int(stack.pop())
-                num2 = int(stack.pop())
-                if num == '*':
-                    stack.append(num2 * num1)
-                if num == '+':
-                    stack.append(num2 + num1)
-                if num == '-':
-                    stack.append(num2 - num1)
-                if num == '/':
-                    stack.append(int(float(num2) / num1))
+
+        def calculate_expression(num1, num2, operator):
+            num1 = int(num1)
+            num2 = int(num2)
+            if operator == "+":
+                return num1 + num2
+            elif operator == "-":
+                return num1 - num2
+            elif operator == "*":
+                return num1 * num2
+            elif operator == "/":
+                return int(num1 / num2)
+
+
+        for i,num in enumerate(tokens):
+            if num in valid_operators:
+                op2 = stack.pop()
+                op1 = stack.pop()
+                res = calculate_expression(op1,op2, num)
+                stack.append(res)
             else:
-                stack.append(int(num))
+                stack.append(num)
 
-        return stack.pop()
-
+        return int(stack[0])
+        
