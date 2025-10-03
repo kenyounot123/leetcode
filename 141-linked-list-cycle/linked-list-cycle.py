91ea062852d57a30c6be7dbfe2eb2a3ff502e8b3
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2025-10-02 21:04:48 -0400

    Time: 55 ms (16.50%) | Memory: 19.9 MB (57.40%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        # return a boolean depending on whether the linked list has a cycle in it or not
+        # cycle exists if by following the next pointer, a node will be reached again
+        # then we can have two pointers one ahead of the other, if it laps the slow pointer it is a cycle
+
+        if head is None or head.next is None:
+            return False
+        
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+
+        return False
