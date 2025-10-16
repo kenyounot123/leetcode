69cf5e15d81bf5241a424fafa43e1963b5ac796c
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2025-10-16 17:55:57 -0400

    Time: 1 ms (43.03%) | Memory: 17.8 MB (92.49%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,19 +1,26 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         # Given n pair of parentheses
-        output = []
-        
-        def dfs(open_p, close_p, s):
-            if open_p == close_p and open_p + close_p == n * 2:
-                output.append(s)
-                return
-            
-            if open_p < n:
-                dfs(open_p + 1, close_p, s + "(")
-            if close_p < open_p:
-                dfs(open_p, close_p + 1, s + ")")
-        
-        dfs(0,0, "")
+        # generate all combinations => recursion
 
-        return output
+        result = []
+
+        def dfs(s, open, close):
+            if open + close == n * 2:
+                result.append(s)
             
+            if open < n:
+                dfs(s + "(", open + 1, close)
+            if close < open:
+                dfs(s + ")", open, close + 1)
+            
+            
+
+        dfs("", 0,0)
+
+
+        return result
+
+
+        
+        
