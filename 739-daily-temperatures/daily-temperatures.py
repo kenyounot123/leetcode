e209377ea160218d2e110267b6fd271e4145b4da
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2025-10-17 21:17:39 -0400

    Time: 166 ms (6.34%) | Memory: 35.4 MB (5.52%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.py
@@ -0,0 +1,25 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # return an array answer
+        # answer[i] is the number of days you have to wait after the ith day to get 
+        # a warmer temp or 0 if no warmer temp
+
+        temperatures.append(0)
+        answer = [0] * len(temperatures)
+        stack = []
+
+        for i,temp in enumerate(temperatures):
+            if len(stack) == 0:
+                stack.append([i,temp])
+                continue
+            
+            while stack and temp > stack[-1][1]:
+                stack_index, stack_temp = stack.pop()
+                answer[stack_index] = i - stack_index
+            
+
+            stack.append([i, temp])
+        
+        answer.pop()
+
+        return answer
