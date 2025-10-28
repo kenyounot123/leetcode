a9f6bd7c83ab6258817fde404a0ef5c5110b9ad8
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2025-10-27 22:08:16 -0400

    Time: 180 ms (29.03%) | Memory: 37.5 MB (5.66%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -1,25 +1,26 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
         # inputs: integers heights[]
-        # outputs: return an area int
-        # area = heights[i] * i 
-        # we need to find a valid i
+        # return area of largest rectangle
 
-        stack = [[0,heights[0]]]
+        stack = [[0, heights[0]]]
         max_area = 0
 
-        for i in range(1,len(heights)): 
-            if heights[i] >= stack[-1][1]:
-                stack.append([i,heights[i]])
+        for idx, height in enumerate(heights):
+            if height >= stack[-1][1]:
+                stack.append([idx, height])
             else:
-                while stack and heights[i] < stack[-1][1]:
-                    idx,height = stack.pop()
-                    current_area = height * (i - idx)
-                    max_area = max(max_area, current_area)
-                stack.append([idx, heights[i]])
+                while stack and height < stack[-1][1]:
+                    i, h = stack.pop()
+                    area = (idx - i) * h 
+                    max_area = max(max_area, area)
+                stack.append([i, height])
+
 
         for i, h in stack:
             width = len(heights) - i
-            max_area = max(max_area, h * width)
-        
+            area = width * h
+            max_area = max(max_area, area)
+
         return max_area
+
