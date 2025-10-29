bf466d042850de8d2463a28a8d61d31529359be8
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2025-10-28 21:15:00 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (12.76%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -7,13 +7,16 @@ class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         # given head of linked list and an integer n
         # return head of linked list after removing nth node FROM THE END of the list
-        # [1,2,3,4,5]
+        # to remove nodes we want to do head.next = head.next.next
+        # so as long as we find the node right before the nth node from the end
+        # we can remove easily.
+
         result = ListNode(0, head)
         dummy = result
 
         for _ in range(n):
             head = head.next
-
+        
         while head:
             head = head.next
             dummy = dummy.next
@@ -21,4 +24,3 @@ class Solution:
         dummy.next = dummy.next.next
 
         return result.next
-
