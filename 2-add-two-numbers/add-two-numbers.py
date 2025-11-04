24d3204d731327e0ab29ad5b93b9e598ee4bdcfc
Ken Lu <70028795+kenyounot123@users.noreply.github.com>
2025-11-04 18:53:02 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (56.73%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -9,28 +9,27 @@ class Solution:
         # output: a linked list representing the sum of the digits
         # 1 -> 2 -> 3
         # 1 -> 2 -> 3
-        # 2 -> 4 -> 6
-        dummy = ListNode()
-        curr = dummy
+
         carry = 0
+        current = ListNode(0, None)
+        dummy = ListNode(0, current)
 
         while l1 or l2 or carry:
-            d1 = l1.val if l1  else 0
+            d1 = l1.val if l1 else 0
             d2 = l2.val if l2 else 0
             total = d1 + d2 + carry
-            d3 = ListNode(total % 10)
+            digit = total % 10
             carry = total // 10
 
-            curr.next = d3
-            curr = curr.next
+            new_node = ListNode(digit)
+
+            current.next = new_node
+            current = current.next
 
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
         
-        return dummy.next
 
-
-       
-
+        return dummy.next.next
         
-        
+
